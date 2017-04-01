ce9bf2a71e300565a2667d44d20e223cd0f83068
mysteryguy123 <benfowler2012@gmail.com>
2017-04-01 16:15:30 -0700

    Created wait script in a while loop

--- CFWaitCondition.sh
+++ CFWaitCondition.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-ping -c 4 -W 1 8.8.8.8
 
-RESULT=$?
 
-if [ "$RESULT" == 0 ]
- then	
-	curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "SUCCESS","Reason" : "ControlBox has internet access","UniqueId" : "ID1234","Data" : "Application has completed configuration."}' "$SignalURL"
-else
-	curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "ControlBox does NOT have interent access","UniqueId" : "ID1235","Data" : "Application has NOT completed configuration."}' "$SignalURL"
-fi
+START=`date +%s`
+while [ $(( $(date +%s) - 300 )) -lt $START ]; do
+	ping -c 4 -W 1 8.8.8.8
+	RESULT=$?
+
+	if [ "$RESULT" == 0 ]
+	 then	
+		curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "SUCCESS","Reason" : "ControlBox has internet access","UniqueId" : "ID1234","Data" : "Application has completed configuration."}' "$SignalURL"
+	else
+		curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "ControlBox does NOT have interent access","UniqueId" : "ID1235","Data" : "Application has NOT completed configuration."}' "$SignalURL"
+	fi
+done
+
